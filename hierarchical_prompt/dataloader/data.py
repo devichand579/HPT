ae7258f543b67c6a73c4dabd8544072f280263bf
Devichand <devichand579@gmail.com>
2024-05-24 17:55:28 +0530

    eval

---
+++ hierarchical_prompt/dataloader/data.py
@@ -0,0 +1,27 @@
+"""
+ An utility class for loading the datasets for corresponding tasks.
+"""
+
+
+from datasets import load_dataset
+
+class DatasetLoader:
+    def __init__(self):
+        self.datasets = {
+            "qa": None,
+            "reasoning": None,
+            "french": None,
+            "summarization": None
+        }
+    
+    def load_datasets(self):
+        # Load the datasets and store them in the dictionary
+        self.datasets["qa"] = load_dataset("google/boolq", split="validation")
+        self.datasets["reasoning"] = load_dataset("tau/commonsense_qa", split="validation")
+        self.datasets["french"] = load_dataset("iwslt2017", "iwslt2017-en-fr", split="validation")
+        self.datasets["summarization"] = load_dataset("samsum", split="test")
+    
+    def get_dataset(self, name):
+        if self.datasets[name] is None:
+            self.load_datasets()
+        return self.datasets.get(name, f"Dataset '{name}' not found")
---
+++ hierarchical_prompt/metrics/rouge/__init__.py
@@ -0,0 +1,2 @@
+from .rouge import Rouge
+from .rouge import Tokenizer
