70219db53445a96357eefe80b9f332c0a826d954
Devichand <devichand579@gmail.com>
2024-05-26 22:48:06 +0530

    main

--- hierarchical_prompt/main.py
+++ hierarchical_prompt/main.py
@@ -4,3 +4,6 @@ from models import LLama3, Gemma, Phi3, Mistral
 from prompts import Roleprompt, ZeroshotCoT, threeshotCoT, Leasttomost, generatedknowledge
 from utils import AnswerProcessor
 
+
+def main():
+
